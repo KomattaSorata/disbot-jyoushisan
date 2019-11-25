f4966fc3510d5d85c0dc4fae5d95d2087a52c615
Sorata Fujitsuka <sorata.fujitsuka@gmail.com>
2019-11-25 10:51:23 +0900

    Updated Tweets fetching method

--- fetchtweet.js
+++ fetchtweet.js
@@ -8,28 +8,40 @@ module.exports = () => {
         access_token_key: mibunshou.tw_acctoken,
         access_token_secret: mibunshou.tw_acctokensecret
     });
-    const params = {screen_name: 'komattasorata', count:200, exclude_replies: true, include_rts: false};
-    client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        let rescount = tweets.length;
-        let i;
-        for(i = 0; i < rescount; i++){
-            let text = tweets[i].text;
+
+    const since_tid_sorata = fs.readFileSync('./src/mkgen_source/since_id_sorata.txt', 'UTF-8');
+
+    const params_sorata = {screen_name: 'komattasorata', count:200, exclude_replies: true, include_rts: false, since_id: since_tid_sorata};
+    client.get('statuses/user_timeline', params_sorata, function(error, tweets, response) {
+    if (!error) {
+        fs.writeFile('./since_id_sorata.txt', tweets[0].id, (err) => {
+            if(err) throw err; 
+        });
+        tweets.map(resp => {
+            let text = resp.text;
             var handled = text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-            fs.writeFile('./src/mkgen_source/sorata.txt', `${handled} \r\n`, (err) => {
+            fs.appendFile('./src/mkgen_source/sorata.txt', `${handled} \r\n`, (err) => {
                 if (err) throw err;
             });
-        }
+        });
+    }
     });
-    const params_josh = {screen_name: 'joshanska', count:200, exclude_replies: true, include_rts: false};
+
+    const since_tid_josh = fs.readFileSync('./src/mkgen_source/since_id_josh.txt', 'UTF-8');
+
+    const params_josh = {screen_name: 'joshanska', count:200, exclude_replies: true, include_rts: false, since_id: since_tid_josh};
     client.get('statuses/user_timeline', params_josh, function(error, tweets, response) {
-        let rescount = tweets.length;
-        let i;
-        for(i = 0; i < rescount; i++){
-            let text = tweets[i].text;
+    if (!error) {
+        fs.writeFile('./since_id_josh.txt', tweets[0].id, (err) => {
+            if(err) throw err; 
+        });
+        tweets.map(resp => {
+            let text = resp.text;
             var handled = text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
-            fs.writeFile('./src/mkgen_source/josh.txt', `${handled} \r\n`, (err) => {
+            fs.appendFile('./src/mkgen_source/josh.txt', `${handled} \r\n`, (err) => {
                 if (err) throw err;
             });
-        }
+        });
+    }
     });
-};
+}
